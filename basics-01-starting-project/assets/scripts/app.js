a41d82886b2f1c89ae2e4bdc7b3b4aea12307cb2
Eldaw, Elmustafa <mustafa.eldaw@gmail.com>
2022-04-28 21:36:52 +0800

    chenge 10 to20

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,7 +1,7 @@
 const defultResult = 0;
 let currentResult = defultResult;
 
-currentResult = currentResult + 10;
+currentResult = currentResult + 20;
 
 let calculationDescription = `${defultResult}'s ba irthday ` 
 outputResult (currentResult,calculationDescription); 
