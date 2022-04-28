5ed74d5f54dd18b0f2912d315a47eed8a3136081
Eldaw, Elmustafa <mustafa.eldaw@gmail.com>
2022-04-28 21:33:12 +0800

    first commit

---
+++ basics-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,7 @@
+const defultResult = 0;
+let currentResult = defultResult;
+
+currentResult = currentResult + 10;
+
+let calculationDescription = `${defultResult}'s ba irthday ` 
+outputResult (currentResult,calculationDescription); 
